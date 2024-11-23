4c28d8034f2f3c081726e3a1f6078387e2187b34
dbswl701 <dbswl701@ajou.ac.kr>
2024-11-23 05:41:03 +0000

    design(front): 게시글 작성 UI

--- frontend/src/pages/Write/Write.tsx
+++ frontend/src/pages/Write/Write.tsx
@@ -1,26 +1,52 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './Write.module.scss';
 import MDEditor, { MDEditorProps } from '@uiw/react-md-editor';
 import styled from 'styled-components';
 import { usePostCreatePost } from '@hooks/usePostCreatePost';
+import remarkBreaks from 'remark-breaks';
+import { GrPrevious } from 'react-icons/gr';
 
 const EditorContainer = styled.div`
 	width: 100vw;
 	height: 90vh;
 	padding: 1rem;
 	box-sizing: border-box;
-	background-color: #f8f9fa; /* 원하는 배경색으로 변경 가능 */
+	// background-color: #f8f9fa; /* 원하는 배경색으로 변경 가능 */
 
 	@media (max-width: 768px) {
 		padding: 0.5rem;
 	}
 `;
 
+const StyledEditor = styled(MDEditor)`
+	background-color: #ffffff !important; /* 흰색 배경 */
+	color: var(--color-fg-default); /* 텍스트 색상 */
+
+	.w-md-editor-toolbar {
+		background-color: #ffffff !important; /* 툴바 배경 */
+		color: black !important;
+	}
+	.w-md-editor-preview > .wmde-markdown {
+		background-color: #ffffff !important; /* 미리보기 배경 */
+		color: black !important; /* 텍스트 색상 */
+	}
+`;
+
 const Write = () => {
 	const [title, setTitle] = useState('');
 	const [value, setValue] = useState('**Hello world!!!**');
+	const [isMobile, setIsMobile] = useState(false);
 
 	const { mutate, isLoading } = usePostCreatePost();
+	// 모바일 여부 감지
+	useEffect(() => {
+		const handleResize = () => {
+			setIsMobile(window.innerWidth <= 768);
+		};
+		handleResize();
+		window.addEventListener('resize', handleResize);
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
 
 	const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
@@ -32,8 +58,16 @@ const Write = () => {
 
 		mutate({ title: title.trim(), contents: value.trim() });
 	};
+
 	return (
 		<EditorContainer>
+			<div className="d-flex justify-content-between mt-2">
+				<div className={styles.Write__previousBtn}>
+					<GrPrevious />
+				</div>
+
+				<button className={styles.Write__submitBtn}>등록</button>
+			</div>
 			<form onSubmit={onSubmit}>
 				<div className={styles.Write__header}>
 					<textarea
@@ -41,13 +75,18 @@ const Write = () => {
 						onChange={(e) => setTitle(e.target.value)}
 						placeholder="제목을 입력하세요"
 						className={styles.Write__title}
+						maxLength={50}
+						rows={2}
 					/>
-					<button className={styles.Write__submitBtn}>등록</button>
 				</div>
-				<MDEditor
+				<StyledEditor
 					value={value}
 					onChange={setValue as MDEditorProps['onChange']}
-					height="100%"
+					previewOptions={{
+						remarkPlugins: [remarkBreaks],
+					}}
+					height={'67vh'}
+					preview={isMobile ? 'edit' : 'live'}
 				/>
 			</form>
 		</EditorContainer>
