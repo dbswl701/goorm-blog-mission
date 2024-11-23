3d591dfab644bad52ffc9d84106340ebb5fd316c
dbswl701 <dbswl701@ajou.ac.kr>
2024-11-23 00:12:38 +0000

    feat(back): 댓글 수정 및 삭제 api 개발

--- backend/src/routes/comment.ts
+++ backend/src/routes/comment.ts
@@ -1,6 +1,11 @@
 import express, { Response, Request, NextFunction } from 'express';
 import { authenticate } from '../middleware/auth';
-import { createComment, getCommentsByPostId } from '../services/comment';
+import {
+	createComment,
+	deleteComment,
+	getCommentsByPostId,
+	updateComment,
+} from '../services/comment';
 
 const router = express.Router({ mergeParams: true }); // 부모 라우트의 params를 병합
 
@@ -50,4 +55,50 @@ router.get(
 	}
 );
 
+// 댓글 수정
+router.put(
+	'/:commentId',
+	authenticate,
+	async (req: Request, res: Response, next: NextFunction) => {
+		const { commentId } = req.params;
+		const userId = req.session.user!.id;
+		const userName = req.session.user!.username;
+		const { content } = req.body;
+
+		try {
+			const updatedComment = await updateComment(
+				commentId,
+				userId,
+				userName,
+				content
+			);
+			res.status(200).json({
+				success: true,
+				data: updatedComment,
+			});
+		} catch (error) {
+			next(error);
+		}
+	}
+);
+
+// 댓글 삭제
+router.delete(
+	'/:commentId',
+	authenticate,
+	async (req: Request, res: Response, next: NextFunction) => {
+		const { commentId } = req.params;
+		const userId = req.session.user!.id;
+
+		try {
+			await deleteComment(commentId, userId);
+			res.status(200).json({
+				success: true,
+				data: { message: '댓글이 성공적으로 삭제되었습니다.' },
+			});
+		} catch (error) {
+			next(error);
+		}
+	}
+);
 export default router;
--- backend/src/services/comment.ts
+++ backend/src/services/comment.ts
@@ -5,6 +5,7 @@ import { isValidObjectId, isValidString } from '../utils/validation';
 import postModel from '../models/post/schema';
 import { NotFoundError } from '../errors/NotFoundError';
 import commentModel from '../models/comment/schema';
+import userModel from '../models/user/schema';
 
 // 댓글 생성
 export const createComment = async (
@@ -77,4 +78,79 @@ export const getCommentsByPostId = async (
 		createdAt: comment.createdAt,
 		updatedAt: comment.updatedAt,
 	}));
+};
+
+// 댓글 수정
+export const updateComment = async (
+	commentId: string,
+	userId: string,
+	userName: string,
+	content: string
+): Promise<CommentInterface> => {
+	// 유효성 검사
+	if (!mongoose.Types.ObjectId.isValid(commentId)) {
+		throw new BadRequestError('유효하지 않은 게시글 ID입니다.');
+	}
+	if (!mongoose.Types.ObjectId.isValid(userId)) {
+		throw new BadRequestError('유효하지 않은 댓글 ID입니다.');
+	}
+	if (!isValidString(userName)) {
+		throw new BadRequestError('유효하지 않은 사용자 이름입니다.');
+	}
+	if (!isValidString(content)) {
+		throw new BadRequestError('댓글 내용은 빈 문자열일 수 없습니다.');
+	}
+
+	// 댓글 조회
+	const comment = await commentModel.findById(commentId).exec();
+	if (!comment) {
+		throw new NotFoundError('댓글이 존재하지 않습니다.');
+	}
+
+	// 댓글 작성자 확인
+	if (comment.author.toString() !== userId) {
+		throw new NotFoundError('수정 권한이 없습니다.'); // -> 권한이면 다른 에러코드 아닌가?
+	}
+
+	// 댓글 업데이트
+	comment.content = content.trim();
+	const updatedComment = await comment.save();
+
+	return {
+		id: updatedComment._id.toString(),
+		postId: updatedComment.postId.toString(),
+		author: userName,
+		content: updatedComment.content,
+		createdAt: updatedComment.createdAt,
+		updatedAt: updatedComment.updatedAt,
+	};
+};
+
+// 댓글 삭제
+export const deleteComment = async (
+	commentId: string,
+	userId: string
+): Promise<void> => {
+	// 유효성 검사
+	if (!mongoose.Types.ObjectId.isValid(commentId)) {
+		throw new BadRequestError('유효하지 않은 게시글 ID입니다.');
+	}
+	if (!mongoose.Types.ObjectId.isValid(userId)) {
+		throw new BadRequestError('유효하지 않은 댓글 ID입니다.');
+	}
+
+	// 댓글 조회
+	const comment = await commentModel.findById(commentId).exec();
+	if (!comment) {
+		throw new NotFoundError('댓글이 존재하지 않습니다.');
+	}
+
+	// 댓글 작성자 확인
+	console.log('[model] comment: ', comment);
+	if (comment.author.toString() !== userId) {
+		throw new NotFoundError('삭제 권한이 없습니다.'); // -> ? 다른 에러 아닌가?
+	}
+
+	// 댓글 삭제
+	await commentModel.deleteOne({ _id: commentId }).exec();
 };
