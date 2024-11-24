458fc90cc683ff5ea64bb1300cac973cf568e12c
dbswl701 <dbswl701@ajou.ac.kr>
2024-11-24 14:54:33 +0000

    design(front): 검색 버튼 디자인 수정

--- frontend/src/components/Header/SearchBar.tsx
+++ frontend/src/components/Header/SearchBar.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import styles from './SearchBar.module.scss';
 import { useSearchParams } from 'react-router-dom';
 import AutoComplete from '@components/AutoComplete';
-
+import './Header.css';
 const SearchBar = () => {
 	const [search, setSearch] = useState('');
 	const [filter, setFilter] = useState('all');
