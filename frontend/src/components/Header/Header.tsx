375178234114ea5d0bf230508bb0a80dc2c37ccd
dbswl701 <dbswl701@ajou.ac.kr>
2024-11-24 12:11:43 +0000

    design(front): 헤더 디자인 수정

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,13 +1,12 @@
-import HeaderTitle from '@components/Header/HeaderTitle';
-import HeaderUser from '@components/Header/HeaderUser';
 import useAuth from '@hooks/useAuth';
 import axios from 'axios';
 import { useState } from 'react';
 import { FaPlus, FaSearch, FaTimes, FaUserCircle } from 'react-icons/fa';
 import ProfileMenu from './ProfileMenu';
 import SearchBar from './SearchBar';
-import './Header.css';
 import { useNavigate } from 'react-router-dom';
+import styles from './Header.module.scss';
+
 const Header = () => {
 	const username = useAuth();
 	const navigate = useNavigate();
@@ -25,6 +24,7 @@ const Header = () => {
 	const toggleSearch = () => {
 		setIsSearchOpen((prev) => !prev);
 	};
+
 	const onLogout = () => {
 		axios.get('/auth/logout').then(({ data }) => {
 			if (data?.result) {
@@ -34,22 +34,43 @@ const Header = () => {
 	};
 
 	return (
-		<header className="fixed-top">
-			<nav className="navbar navbar-light bg-light px-4 position-relative">
-				<a className="navbar-brand" href="/">
+		<header className={styles.header}>
+			<nav className={styles.container}>
+				<a className={styles.logo} href="/">
 					goorm Blog
 				</a>
-				{/* <HeaderUser username={username} onLogout={onLogout} /> */}
-				<div>
-					<button onClick={toggleSearch}>
+
+				<div className={styles.menu}>
+					<button
+						className={styles.iconButton}
+						onClick={toggleSearch}
+						aria-label={
+							isSearchOpen ? '검색창 닫기' : '검색창 열기'
+						}
+						aria-expanded={isSearchOpen}
+					>
 						{isSearchOpen ? <FaTimes /> : <FaSearch />}
 					</button>
+
 					{username ? (
 						<>
-							<button onClick={() => navigate('/write')}>
+							<button
+								className={styles.iconButton}
+								onClick={() => navigate('/write')}
+								aria-label="게시글 작성하기"
+							>
 								<FaPlus />
 							</button>
-							<button onClick={toggleProfileMenu}>
+							<button
+								className={styles.iconButton}
+								onClick={toggleProfileMenu}
+								aria-label={
+									isProfileMenuOpen
+										? '프로필 메뉴 닫기'
+										: '프로필 메뉴 열기'
+								}
+								aria-expanded={isProfileMenuOpen}
+							>
 								<FaUserCircle />
 							</button>
 							{isProfileMenuOpen && (
@@ -61,15 +82,14 @@ const Header = () => {
 							)}
 						</>
 					) : (
-						<a className="nav-link" href="/login">
+						<a
+							className={styles.iconButton}
+							href="/login"
+							aria-label="로그인 페이지로 이동"
+						>
 							Login
 						</a>
 					)}
-				</div>
-				<div
-					className={`search-bar-container ${isSearchOpen ? 'open' : ''}`}
-				>
-					{isSearchOpen && <SearchBar />}
 				</div>
 			</nav>
 		</header>
