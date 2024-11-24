5540f614c1b55c1513eefdf9e744c0b327413cb3
dbswl701 <dbswl701@ajou.ac.kr>
2024-11-24 13:14:18 +0000

    fix(front): 게시글 단건 조회에서 새로고침 되던 문제 해결

--- frontend/src/components/PostListItem/PostListItem.tsx
+++ frontend/src/components/PostListItem/PostListItem.tsx
@@ -1,8 +1,4 @@
-import React from 'react';
-import { formatDate } from 'utils/formatDate';
-import styles from './PostListItem.module.scss';
-import { AiOutlineLike } from 'react-icons/ai';
-import { BiCommentDetail } from 'react-icons/bi';
+import { Link } from 'react-router-dom';
 
 interface PostListItemProps {
 	id: string;
@@ -14,7 +10,7 @@ interface PostListItemProps {
 	commentCount: number;
 }
 
-const PostListItem = ({
+const PostItem = ({
 	id,
 	title,
 	createdAt,
@@ -22,35 +18,25 @@ const PostListItem = ({
 	author,
 	likeCount,
 	commentCount,
-}: PostListItemProps) => {
-	return (
-		<a
-			href={`/posts/${id}`}
-			className={`list-group-item list-group-item-action ${styles.postListItem}`}
-			aria-current="true"
-			aria-describedby={`post-summary-${id}`}
-			role="article"
-		>
-			<div className={styles.postHeader}>
-				<h5 id={`post-title-${id}`}>{title}</h5>
-				<small>{formatDate(createdAt)}</small>
+}: PostListItemProps) => (
+	<Link
+		to={`/posts/${id}`}
+		className="list-group-item list-group-item-action"
+		aria-current="true"
+	>
+		<div className="d-flex w-100 justify-content-between">
+			<h5 className="mb-1">{title}</h5>
+			<small>{new Date(createdAt).toLocaleDateString()}</small>
+		</div>
+		<p className="mb-1">{summary}</p>
+		<div className="d-flex justify-content-between align-items-center">
+			<small>by {author}</small>
+			<div>
+				<small>{likeCount} Likes</small>
+				<small>{commentCount} Comments</small>
 			</div>
-			<p id={`post-summary-${id}`} className={styles.postSummary}>
-				{summary.length > 100 ? `${summary.slice(0, 100)}...` : summary}
-			</p>
-			<div className={styles.postAuthorAndStats}>
-				<small className={styles.author}>by {author}</small>
-				<div className={styles.stats}>
-					<small>
-						<AiOutlineLike /> {likeCount}
-					</small>
-					<small>
-						<BiCommentDetail /> {commentCount}
-					</small>
-				</div>
-			</div>
-		</a>
-	);
-};
+		</div>
+	</Link>
+);
 
-export default PostListItem;
+export default PostItem;
